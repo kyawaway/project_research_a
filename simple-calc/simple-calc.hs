d8ace73ed4424cc5ab3c0910a1f9ce7792a15221
kya <tkmz0216@gmail.com>
2021-04-26 03:00:10 +0900

    creating ast

--- simple-calc/simple-calc.hs
+++ simple-calc/simple-calc.hs
@@ -5,6 +5,19 @@ import qualified Text.Parsec.Language as Lang
 
 type Parser a = Parsec String () a
 
+
+-- ast 
+data Expr = Const Integer
+          | Add Expr Expr
+          | Sub Expr Expr
+          | Mul Expr Expr
+          | Div Expr Expr
+          | Pow Expr Expr
+          | Fact Expr
+          | Negate Expr
+          deriving (Eq, Show)
+-- lexer
+
 symbol :: String -> Parser String
 symbol = TT.symbol Lang.haskell
 
@@ -16,21 +29,50 @@ reservedOp = TT.reservedOp Lang.haskell
 natural :: Parser Integer
 natural = TT.natural Lang.haskell
 
-atom :: Parser Integer
+whiteSpace :: Parser ()
+whiteSpace = TT.whiteSpace Lang.haskell
+
+-- parse
+
+--  ( exp ) | num
+atom :: Parser Expr
 atom = do symbol "("
           x <- expr
           symbol ")"
           return x
-   <|> natural
+   <|> (Const <$> natural)
 
-expr :: Parser Integer
+
+
+-- evaluate whole expressions 
+wholeExpr :: Parser Expr
+wholeExpr = do whiteSpace
+               x <- expr
+               eof
+               return x
+
+
+-- operator
+expr :: Parser Expr
 expr = buildExpressionParser
-        [[binary "*" (*) AssocLeft, binary "/" div AssocLeft]
-        ,[binary "+" (+) AssocLeft, binary "-" (-) AssocLeft]
-        ]
-        atom
-    where
-      binary name fun assoc = Infix (reservedOp name >> return fun) assoc
+       [[binary "^" Pow AssocRight]
+       ,[binary "*" Mul AssocLeft, binary "/" Div AssocLeft]
+       ,[binary "+" Add AssocLeft, binary "-" Sub AssocLeft, prefix "-" Negate]
+       ]
+       atom
+  where
+    binary name fun assoc = Infix (reservedOp name >> return fun) assoc
+    prefix name fun = Prefix (fun <$ reservedOp name)
+
+-- evaluate ast
+eval :: Expr -> Integer
+eval (Const x) = x
+eval (Add x y) = eval x + eval y
+eval (Sub x y) = eval x - eval y
+eval (Mul x y) = eval x * eval y
+eval (Div x y) = eval x `div` eval y
+eval (Pow x y) = eval x ^ eval y
+eval (Negate x) = - eval x
 
 
 main :: IO ()
@@ -38,5 +80,6 @@ main = do putStrLn "Enter expression:"
           s <- getLine
           case parse expr "stdin" s of
             Left err -> print err
-            Right x -> print x
+            Right x -> do print x
+                          print (eval x)
 
