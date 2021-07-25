bc2ecff2fe4efb99a703dc04c4b80908ef65d2cf
kya <tkmz0216@gmail.com>
2021-07-25 23:15:29 +0900

    fix

---
+++ rec/Syntax.hs
@@ -0,0 +1,29 @@
+module Syntax where
+
+data Statement = Seq [Statement]
+          | If Expr Statement Statement
+          | While Expr Statement  
+          | Assign String Expr
+          | Return Expr
+          | Skip
+          deriving (Eq,Show)
+
+
+data Expr =  Var String   
+          |  Integer Integer 
+          |  Bool Bool
+          |  Func String Statement 
+          |  Apply Expr Expr
+          |  Negative Expr
+          |  Pow Expr Expr
+          |  Mul Expr Expr
+          |  Div Expr Expr
+          |  Add Expr Expr
+          |  Sub Expr Expr
+          |  Greater Expr Expr
+          |  Less Expr Expr
+          |  Equal Expr Expr
+          deriving (Eq,Show)
+
+-- data Op
+
